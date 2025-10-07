69bb9b4bddddf42aed4059de9494b726815bdafd
Christian Montoya <chrimonlo@gmail.com>
2025-10-07 15:28:46 -0500

    feat(nav): Add the menu icons

--- src/components/molecules/Nav.tsx
+++ src/components/molecules/Nav.tsx
@@ -4,39 +4,46 @@ import FavoritesIcon from '../atoms/Icon/Favorites-icon';
 
 export default function Nav() {
 	return (
-		<div className='w-full flex justify-center'>
-			<nav
-				role='navigation'
-				aria-label='Primary'
-				className='bg-[var(--blue)] rounded-xl px-4 py-2 w-[221px] h-[35px]'
-			>
-				<ul className='flex items-center justify-center gap-9'>
-					<li>
-						<HomeIcon
-							width={20}
-							height={21}
-							color={'var(--gray)'}
-							className='cursor-pointer'
-						/>
-					</li>
-					<li>
-						<SearchIcon
-							width={20}
-							height={21}
-							color={'var(--gray)'}
-							className='cursor-pointer'
-						/>
-					</li>
-					<li>
-						<FavoritesIcon
-							width={20}
-							height={19}
-							color={'var(--gray)'}
-							className='cursor-pointer'
-						/>
-					</li>
-				</ul>
-			</nav>
-		</div>
+		<nav
+			role='navigation'
+			aria-label='Primary'
+			className='bg-[var(--blue)] rounded-lg w-[221px] h-[50px]'
+		>
+			<div className='flex h-full items-center justify-center gap-9'>
+				<a
+					className='cursor-pointer'
+					href=''
+				>
+					<HomeIcon
+						width={20}
+						height={21}
+						color={'var(--gray)'}
+						className='cursor-pointer'
+					/>
+				</a>
+				<a
+					className='cursor-pointer'
+					href=''
+				>
+					<SearchIcon
+						width={20}
+						height={21}
+						color={'var(--gray)'}
+						className='cursor-pointer'
+					/>
+				</a>
+				<a
+					className='cursor-pointer'
+					href=''
+				>
+					<FavoritesIcon
+						width={20}
+						height={19}
+						color={'var(--gray)'}
+						className='cursor-pointer'
+					/>
+				</a>
+			</div>
+		</nav>
 	);
 }
