64adf0bc32b298b4b12448bb20de4c5c3d93cdea
Christian Montoya <chrimonlo@gmail.com>
2025-10-09 16:30:26 -0500

    feat(api.ts): Add the environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
+const API_KEY = import.meta.env.PUBLIC_OPENWEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const API_ENDPOINTS = {
