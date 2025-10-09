206a51119a4e097878c6d4f9b1a6a87f2d343e69
Christian Montoya <chrimonlo@gmail.com>
2025-10-09 16:27:00 -0500

    feat(Home): Delete the api key console.log

--- src/components/organisms/Home.tsx
+++ src/components/organisms/Home.tsx
@@ -2,9 +2,8 @@ import { useFetch } from '../../hooks/useFetch';
 import { API_ENDPOINTS } from '../../services/api';
 
 export default function HomeComponent() {
-	const url = API_ENDPOINTS.CURRENT_WEATHER('London,uk');
+	const url = API_ENDPOINTS.CURRENT_WEATHER('london');
 	const data = useFetch(url);
-
 	if (data.data) {
 		console.log('Datos del clima:', data);
 	}
