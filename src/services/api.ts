6ce3ce3a6b3bfa3735f23c528c01b46fbd2df174
Christian Montoya <chrimonlo@gmail.com>
2025-10-09 16:18:36 -0500

    feat(api.ts): Add the api key environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
-const API_KEY = import.meta.env.OPENWEATHER_API_KEY;
+const API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const API_ENDPOINTS = {
-    CURRENT_WEATHER: (city:string) => `${BASE_URL}/weather?q=${city}&appid=2c08fac8651123694f93eb54a23246e2&units=metric`
+    CURRENT_WEATHER: (city:string) => `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`
 }
