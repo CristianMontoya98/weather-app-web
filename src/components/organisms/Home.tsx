3ab9e9e9e6192cb4d80755bc176db3149ee0a77c
Christian Montoya <chrimonlo@gmail.com>
2025-10-09 16:19:14 -0500

    feat(Home): Add the fetching with the url created from the api.ts file

--- src/components/organisms/Home.tsx
+++ src/components/organisms/Home.tsx
@@ -1,7 +1,9 @@
 import { useFetch } from '../../hooks/useFetch';
+import { API_ENDPOINTS } from '../../services/api';
 
 export default function HomeComponent() {
-	const data = useFetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2c08fac8651123694f93eb54a23246e2&units=metric');
+	const url = API_ENDPOINTS.CURRENT_WEATHER('London,uk');
+	const data = useFetch(url);
 
 	if (data.data) {
 		console.log('Datos del clima:', data);
