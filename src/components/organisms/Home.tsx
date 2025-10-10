dc48556e53462534326f845af4ed542855978e52
Christian Montoya <chrimonlo@gmail.com>
2025-10-10 12:24:45 -0500

    feat(Home): Add the city name from the api

--- src/components/organisms/Home.tsx
+++ src/components/organisms/Home.tsx
@@ -2,14 +2,18 @@ import { useFetch } from '../../hooks/useFetch';
 import { API_ENDPOINTS } from '../../services/api';
 
 export default function HomeComponent() {
-	const url = API_ENDPOINTS.CURRENT_WEATHER('london');
+	const url = API_ENDPOINTS.CURRENT_WEATHER('Bogotá');
 	const data = useFetch(url);
+
 	if (data.data) {
-		console.log('Datos del clima:', data);
+		console.log('Datos del clima:', data.data);
 	}
+
 	return (
 		<div>
-			<p>Hola home</p>
+			<h2 className='font-bold text-[var(--lightBlue)] text-[20px]'>
+				{(data?.data as any)?.name} {/* <span className='font-normal text-[var(--lightBlueTransparency)]'>Colombia</span> */}
+			</h2>
 		</div>
 	);
 }
