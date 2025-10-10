de97dfb7a96232297bbba9cd5a956503f5bb9bff
Christian Montoya <chrimonlo@gmail.com>
2025-10-10 12:26:05 -0500

    feat(api): Add the language parameter in the url

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,5 +2,5 @@ const API_KEY = import.meta.env.PUBLIC_OPENWEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const API_ENDPOINTS = {
-    CURRENT_WEATHER: (city:string) => `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`
+    CURRENT_WEATHER: (city:string) => `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric&lang=sp,es`
 }
