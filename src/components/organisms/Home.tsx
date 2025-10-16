69439c5968fda7f193a4c5391ab9289ab783361c
Christian Montoya <chrimonlo@gmail.com>
2025-10-16 16:53:05 -0500

    feat(Home): Add the date and time

--- src/components/organisms/Home.tsx
+++ src/components/organisms/Home.tsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from 'react';
 import { API_ENDPOINTS } from '../../services/api';
-import { getUserLocation } from '../../helpers/getUserLocation';
+import { getUserLocation } from '../../helpers/get-user-location';
+import { getUserTime } from '../../helpers/get-user-time';
+import type { UserTime } from '../../types/user';
 
 export default function HomeComponent() {
 	const [city, setCity] = useState<any>(null);
 	const [weatherData, setWeatherData] = useState<any>(null);
+	const [currentTime, setCurrentTime] = useState<UserTime>(getUserTime().userTime);
 
 	useEffect(() => {
 		getUserLocation().then((locationData: any) => {
 			setCity(locationData[0]);
 		});
+		const timer = setInterval(() => {
+			setCurrentTime(getUserTime().userTime);
+		}, 60000);
+
+		return () => clearInterval(timer);
 	}, []);
 
 	useEffect(() => {
@@ -24,13 +32,11 @@ export default function HomeComponent() {
 		}
 	}, [city]);
 
-	if (weatherData) {
-		console.log('Datos del clima:', weatherData);
-	}
-
 	return (
 		<div className='mt-5 flex flex-col items-center justify-center'>
 			<h2 className='font-bold text-[var(--lightBlue)] text-[20px]'>{city?.local_names.es}</h2>
+			<p className='font-bold text-[var(--lighterBlue)] text-[20px]'>{currentTime.date}</p>
+			<p className='font-bold text-[var(--lightBlueTransparency)] text-[20px]'>{currentTime.time}</p>
 			<p className='font-bold text-[var(--lightBlue)] text-[64px]'>{weatherData?.main.temp}°C</p>
 			<p className='font-bold text-[var(--lightBlue)] text-[20px]'>{weatherData?.weather[0].description}</p>
 		</div>
