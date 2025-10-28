8b61a97fb7826411027125337ac5aea07ec051e4
Christian Montoya <chrimonlo@gmail.com>
2025-10-28 09:09:32 -0500

    feat(get-user-location): Add the fetch to the forecast endpoint

--- src/helpers/get-user-location.ts
+++ src/helpers/get-user-location.ts
@@ -7,15 +7,24 @@ export const getUserLocation = async() => {
             async ({coords}) => {
                 const coordinates = [coords.latitude, coords.longitude];
                 let locationData:any;
+                let locationForecastData:any;
+                let data:any;
                 try {
                     const locationUrl = API_ENDPOINTS.CURRENT_LOCATION(coordinates[0],coordinates[1] );
-                    const response = await fetch(locationUrl);
-                    locationData = await response.json();
+                    const locationForecastUrl = API_ENDPOINTS.THREE_HOUR_FORECAST(coordinates[0],coordinates[1] );
+                    const locationResponse = await fetch(locationUrl);
+                    const locationForecastResponse = await fetch(locationForecastUrl);
+                    locationData = await locationResponse.json();
+                    locationForecastData = await locationForecastResponse.json();
+                    data = {
+                        locationData,
+                        locationForecastData
+                    }
                 } catch (error) {
                     console.log('Error al obtener ubicación:', error);
                 }
                 
-                resolve(locationData);
+                resolve(data);
             },
             (err) => {
                 alert('No se pudo obtener la localización')
