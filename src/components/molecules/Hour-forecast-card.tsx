b0f3740854ebe7371fda38fcc32c187d83a263ec
Christian Montoya <chrimonlo@gmail.com>
2025-10-29 16:06:50 -0500

    feat(Hour-forecast-card): Add the icon

--- src/components/molecules/Hour-forecast-card.tsx
+++ src/components/molecules/Hour-forecast-card.tsx
@@ -1,6 +1,13 @@
+import Icons from '../atoms/Icon/WeatherIcons';
+
 export default function HourForecastCard() {
 	return (
 		<div className='bg-[var(--blue)] p-[14px] rounded-[13px] flex flex-col items-center justify-center'>
+			<img
+				className='icon'
+				src={Icons('Thunderstorm')}
+				alt='icon-weather'
+			/>
 			<p className='text-[var(--lighterBlue)] text-[20px]'>4:00 pm</p>
 			<p className='text-[var(--lightBlue)] text-[30px] font-bold'>20°C</p>
 		</div>
