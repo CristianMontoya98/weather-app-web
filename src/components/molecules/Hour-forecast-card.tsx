622897e33b9c2bd95ee6d738b74f6a356bf8d68b
Christian Montoya <chrimonlo@gmail.com>
2025-10-29 15:24:09 -0500

    feat(Hour-forecast-card): Create the hour forecast card component and add styles

---
+++ src/components/molecules/Hour-forecast-card.tsx
@@ -0,0 +1,8 @@
+export default function HourForecastCard() {
+	return (
+		<div className='bg-[var(--blue)] p-[14px] rounded-[13px] flex flex-col items-center justify-center'>
+			<p className='text-[var(--lighterBlue)] text-[20px]'>4:00 pm</p>
+			<p className='text-[var(--lightBlue)] text-[30px] font-bold'>20°C</p>
+		</div>
+	);
+}
