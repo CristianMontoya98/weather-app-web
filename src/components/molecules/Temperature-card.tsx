3fe30ada4f7cd4552697b5bc8ff6b3c289f2795c
Christian Montoya <chrimonlo@gmail.com>
2025-10-31 16:26:06 -0500

    feat(Temperature-card): Change the component name

--- src/components/molecules/Temperature-card.tsx
+++ src/components/molecules/Temperature-card.tsx
@@ -1,10 +1,9 @@
-import Icons from '../atoms/Icon/WeatherIcons';
-interface HourForecastProps {
+interface TemperatureCardProps {
 	title: string;
 	data: string;
 }
 
-export default function HourForecastCard({ title, data }: HourForecastProps) {
+export default function TemperatureCard({ title, data }: TemperatureCardProps) {
 	return (
 		<div className='bg-[var(--blue)] p-[14px] rounded-[13px] flex flex-col items-center justify-center'>
 			<p className='text-[var(--lighterBlue)] text-[20px]'>{title}</p>
