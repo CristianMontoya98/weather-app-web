bd8d8926d3ddeb7581fe6aeb34ae7e43e260980e
Christian Montoya <chrimonlo@gmail.com>
2025-10-31 16:46:48 -0500

    feat(Temperature-card): Add the image

--- src/components/molecules/Temperature-card.tsx
+++ src/components/molecules/Temperature-card.tsx
@@ -1,12 +1,23 @@
 interface TemperatureCardProps {
 	title: string;
 	data: string;
+	image: string;
+	alt: string;
 }
 
-export default function TemperatureCard({ title, data }: TemperatureCardProps) {
+export default function TemperatureCard({ title, data, image, alt }: TemperatureCardProps) {
 	return (
 		<div className='bg-[var(--blue)] p-[14px] rounded-[13px] flex flex-col items-center justify-center'>
-			<p className='text-[var(--lighterBlue)] text-[20px]'>{title}</p>
+			<div className='flex flex-col items-center'>
+				<p className='text-[var(--lighterBlue)] text-[20px]'>{title}</p>
+				<img
+					src={image}
+					alt={alt}
+					width={70}
+					height={70}
+				/>
+			</div>
+
 			<p className='text-[var(--lightBlue)] text-[30px] font-bold'>{data}</p>
 		</div>
 	);
