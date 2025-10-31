d504d5d1c0cefaf6197a81a7eda92b91e299a19b
Christian Montoya <chrimonlo@gmail.com>
2025-10-31 16:28:25 -0500

    feat(api): Delete the Forecast api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,8 @@
 const API_KEY = import.meta.env.PUBLIC_OPENWEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 const GEO_CODING_URL = 'http://api.openweathermap.org/geo/1.0'
-const FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast'
 
 export const API_ENDPOINTS = {
     CURRENT_WEATHER: (city:string) => `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric&lang=sp,es`,
     CURRENT_LOCATION: (lat:any, long:any) => `${GEO_CODING_URL}/reverse?lat=${lat}&lon=${long}&appid=${API_KEY}`,
-    THREE_HOUR_FORECAST:(lat:any, long:any) => `${FORECAST_URL}?lat=${lat}&lon=${long}&cnt=3&appid=${API_KEY}&units=metric&lang=sp,es`
 }
