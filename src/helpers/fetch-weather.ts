cb9d2ec5ea72516c0b303cbb1e08e972941aa536
Christian Montoya <chrimonlo@gmail.com>
2025-11-07 14:39:41 -0500

    feat(fetch-weather): Create the fetch-weather helper

---
+++ src/helpers/fetch-weather.ts
@@ -0,0 +1,13 @@
+export const fetchWeather = (url:string, setData:any, setIcon:any, setIsLoading:any) => {
+    fetch(url)
+				.then((response) => response.json())
+				.then((data) => {
+					setData(data);
+					console.log('weather', data);
+					setIcon(data.weather[0].main);
+					const loadingData = setInterval(() => {
+						setIsLoading(false);
+					}, 1000);
+					return () => clearInterval(loadingData);
+				});
+}
