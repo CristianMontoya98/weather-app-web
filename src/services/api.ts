c6ce4ba258920c419c71f612f16783151995d1ef
Christian Montoya <chrimonlo@gmail.com>
2025-11-12 09:56:48 -0500

    feat(api): Add in the api the spanish language in the endpoint parameters

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,6 @@ const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 const GEO_CODING_URL = 'http://api.openweathermap.org/geo/1.0'
 
 export const API_ENDPOINTS = {
-    CURRENT_WEATHER: (city:string) => `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric&lang=sp,es`,
+    CURRENT_WEATHER: (city:string) => `${BASE_URL}/weather?q=${city}&lang=es&appid=${API_KEY}&units=metric&lang=sp,es`,
     CURRENT_LOCATION: (lat:any, long:any) => `${GEO_CODING_URL}/reverse?lat=${lat}&lon=${long}&appid=${API_KEY}`,
 }
