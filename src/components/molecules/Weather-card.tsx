e3a6838a83a0b511e3f8ee732f7dba9dd15e333f
Christian Montoya <chrimonlo@gmail.com>
2025-11-20 18:00:11 -0500

    feat(Weather-card): Add the styles and show the weather searched data

---
+++ src/components/molecules/Weather-card.tsx
@@ -0,0 +1,24 @@
+import Icons from '../atoms/Icon/Weather-icons';
+
+interface WeatherCardProps {
+	weather: any;
+	icon: string;
+}
+
+export default function WeatherCard({ weather, icon }: WeatherCardProps) {
+	return (
+		<div className='mt-20 cursor-pointer bg-gradient-to-br from-slate-900 to-slate-700 p-[14px] rounded-[13px] flex items-center w-[300px]'>
+			<img
+				className='icon'
+				src={Icons(icon, true)}
+				alt='weather icon'
+				width={90}
+				height={90}
+			/>
+			<div className='bg-[var(--lightBlue)]/60 w-1 h-[120px] rounded-full'></div>
+			<div className='w-full h-full flex flex-col items-center justify-center'>
+				<p className='text-[var(--lightBlue)] text-[20px] font-bold ms-5'>{weather?.name}</p>
+			</div>
+		</div>
+	);
+}
