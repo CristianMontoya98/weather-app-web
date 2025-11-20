92fff5b8b1cf8d3f3e6a0b5586ccd21a4b823c51
Christian Montoya <chrimonlo@gmail.com>
2025-11-20 17:59:34 -0500

    feat(Weather-icons): modify the function to set the icon in the international weather

--- src/components/atoms/Icon/Weather-icons.tsx
+++ src/components/atoms/Icon/Weather-icons.tsx
@@ -1,11 +1,11 @@
-const Icons = (icon: string, time: any) => {
-	if (time === 'day') {
+const Icons = (icon: string, international: boolean, time?: any) => {
+	if (international) {
 		switch (icon) {
 			case 'Thunderstorm':
-				icon = 'src/assets/icons/thunderstorms-day-rain.svg';
+				icon = 'src/assets/icons/thunderstorms-rain.svg';
 				break;
 			case 'Drizzle':
-				icon = 'src/assets/icons/partly-cloudy-day-drizzle.svg';
+				icon = 'src/assets/icons/partly-cloudy-drizzle.svg';
 				break;
 			case 'Rain':
 				icon = 'src/assets/icons/rain.svg';
@@ -20,13 +20,13 @@ const Icons = (icon: string, time: any) => {
 				icon = 'src/assets/icons/weather.svg';
 				break;
 			case 'Clouds':
-				icon = 'src/assets/icons/overcast-day.svg';
+				icon = 'src/assets/icons/overcast.svg';
 				break;
 			case 'Fog':
-				icon = 'src/assets/icons/fog-day.svg';
+				icon = 'src/assets/icons/fog.svg';
 				break;
 			case 'Haze':
-				icon = 'src/assets/icons/haze-day.svg';
+				icon = 'src/assets/icons/haze.svg';
 				break;
 			case 'Smoke':
 				icon = 'src/assets/icons/smoke.svg';
@@ -34,40 +34,77 @@ const Icons = (icon: string, time: any) => {
 			default:
 				icon = 'src/assets/icons/clear-day.svg';
 		}
-	} else if (time === 'night') {
-		switch (icon) {
-			case 'Thunderstorm':
-				icon = 'src/assets/icons/thunderstorms-night-rain.svg';
-				break;
-			case 'Drizzle':
-				icon = 'src/assets/icons/partly-cloudy-night-drizzle.svg';
-				break;
-			case 'Rain':
-				icon = 'src/assets/icons/rain.svg';
-				break;
-			case 'Snow':
-				icon = 'src/assets/icons/snow.svg';
-				break;
-			case 'Clear':
-				icon = 'src/assets/icons/clear-night.svg';
-				break;
-			case 'Atmosphere':
-				icon = 'src/assets/icons/weather.svg';
-				break;
-			case 'Clouds':
-				icon = 'src/assets/icons/overcast-night.svg';
-				break;
-			case 'Fog':
-				icon = 'src/assets/icons/fog-night.svg';
-				break;
-			case 'Haze':
-				icon = 'src/assets/icons/haze-night.svg';
-				break;
-			case 'Smoke':
-				icon = 'src/assets/icons/smoke.svg';
-				break;
-			default:
-				icon = 'src/assets/icons/clear-night.svg';
+	} else {
+		if (time === 'day') {
+			switch (icon) {
+				case 'Thunderstorm':
+					icon = 'src/assets/icons/thunderstorms-day-rain.svg';
+					break;
+				case 'Drizzle':
+					icon = 'src/assets/icons/partly-cloudy-day-drizzle.svg';
+					break;
+				case 'Rain':
+					icon = 'src/assets/icons/rain.svg';
+					break;
+				case 'Snow':
+					icon = 'src/assets/icons/snow.svg';
+					break;
+				case 'Clear':
+					icon = 'src/assets/icons/clear-day.svg';
+					break;
+				case 'Atmosphere':
+					icon = 'src/assets/icons/weather.svg';
+					break;
+				case 'Clouds':
+					icon = 'src/assets/icons/overcast-day.svg';
+					break;
+				case 'Fog':
+					icon = 'src/assets/icons/fog-day.svg';
+					break;
+				case 'Haze':
+					icon = 'src/assets/icons/haze-day.svg';
+					break;
+				case 'Smoke':
+					icon = 'src/assets/icons/smoke.svg';
+					break;
+				default:
+					icon = 'src/assets/icons/clear-day.svg';
+			}
+		} else if (time === 'night') {
+			switch (icon) {
+				case 'Thunderstorm':
+					icon = 'src/assets/icons/thunderstorms-night-rain.svg';
+					break;
+				case 'Drizzle':
+					icon = 'src/assets/icons/partly-cloudy-night-drizzle.svg';
+					break;
+				case 'Rain':
+					icon = 'src/assets/icons/rain.svg';
+					break;
+				case 'Snow':
+					icon = 'src/assets/icons/snow.svg';
+					break;
+				case 'Clear':
+					icon = 'src/assets/icons/clear-night.svg';
+					break;
+				case 'Atmosphere':
+					icon = 'src/assets/icons/weather.svg';
+					break;
+				case 'Clouds':
+					icon = 'src/assets/icons/overcast-night.svg';
+					break;
+				case 'Fog':
+					icon = 'src/assets/icons/fog-night.svg';
+					break;
+				case 'Haze':
+					icon = 'src/assets/icons/haze-night.svg';
+					break;
+				case 'Smoke':
+					icon = 'src/assets/icons/smoke.svg';
+					break;
+				default:
+					icon = 'src/assets/icons/clear-night.svg';
+			}
 		}
 	}
 
