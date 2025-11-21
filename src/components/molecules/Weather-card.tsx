ed2394023645f941a8b967199fa8c082b77295f9
Christian Montoya <chrimonlo@gmail.com>
2025-11-21 09:10:57 -0500

    styles(Weather-card): Add the boxz shadow to the weather data card

--- src/components/molecules/Weather-card.tsx
+++ src/components/molecules/Weather-card.tsx
@@ -7,7 +7,7 @@ interface WeatherCardProps {
 
 export default function WeatherCard({ weather, icon }: WeatherCardProps) {
 	return (
-		<div className='mt-20 cursor-pointer bg-gradient-to-br from-slate-900 to-slate-700 p-[14px] rounded-[13px] flex items-center w-[300px]'>
+		<div className='mt-20 cursor-pointer shadow-[5px_7px_6px_0px_rgba(0,_0,_0,_0.1)] bg-gradient-to-br from-slate-900 to-slate-700 p-[14px] rounded-[13px] flex items-center w-[300px]'>
 			<img
 				className='icon'
 				src={Icons(icon, true)}
