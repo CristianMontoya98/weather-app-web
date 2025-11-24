994b77b8297365da17188f631d2a273bb37b2442
Christian Montoya <chrimonlo@gmail.com>
2025-11-24 14:51:56 -0500

    feat(Search-container): Add the isNotFound state

--- src/components/organisms/Search-container.tsx
+++ src/components/organisms/Search-container.tsx
@@ -4,6 +4,7 @@ import SearchCardsContainer from '../molecules/Search-cards-container';
 
 export default function SearchContainer() {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [isNotFound, setIsNotFound] = useState<boolean>(false);
 	const [icon, setIcon] = useState<string>('');
 	const [weatherData, setWeatherData] = useState<any>(null);
 
@@ -13,11 +14,13 @@ export default function SearchContainer() {
 				setWeatherData={setWeatherData}
 				setIcon={setIcon}
 				setIsLoading={setIsLoading}
+				setIsNotFound={setIsNotFound}
 			/>
 			<SearchCardsContainer
 				icon={icon}
 				isLoading={isLoading}
 				weather={weatherData}
+				isNotFound={isNotFound}
 			/>
 		</div>
 	);
