a112cadb48d2d1f6239ac1adaa85bb216376c42f
Christian Montoya <chrimonlo@gmail.com>
2025-11-24 14:52:15 -0500

    feat(Search-cards-container): Add the isNotFound state

--- src/components/molecules/Search-cards-container.tsx
+++ src/components/molecules/Search-cards-container.tsx
@@ -5,8 +5,10 @@ interface SearchCardsProps {
 	isLoading: boolean;
 	weather: any;
 	icon: string;
+	isNotFound: boolean;
 }
-export default function SearchCardsContainer({ isLoading, weather, icon }: SearchCardsProps) {
+export default function SearchCardsContainer({ isLoading, weather, icon, isNotFound }: SearchCardsProps) {
+	/* console.log(isNotFound); */
 	if (isLoading) {
 		return (
 			<div className='mt-5 flex flex-col items-center justify-center w-screen h-screen'>
@@ -15,14 +17,18 @@ export default function SearchCardsContainer({ isLoading, weather, icon }: Searc
 		);
 	}
 	if (weather) {
-		return (
-			<div className='w-full flex flex-col items-center'>
-				<WeatherCard
-					weather={weather}
-					icon={icon}
-				/>
-			</div>
-		);
+		if (weather?.cod === '404') {
+			return <h1>No se encontraron resultados</h1>;
+		} else {
+			return (
+				<div className='w-full flex flex-col items-center'>
+					<WeatherCard
+						weather={weather}
+						icon={icon}
+					/>
+				</div>
+			);
+		}
 	}
 	return (
 		<div className='w-full flex flex-col items-center mt-[100px]'>
