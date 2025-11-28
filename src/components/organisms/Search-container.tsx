a375222e92cec07db718054204237180b287f710
Christian Montoya <chrimonlo@gmail.com>
2025-11-28 15:23:49 -0500

    feat(Search-container): Add the showDetail state

--- src/components/organisms/Search-container.tsx
+++ src/components/organisms/Search-container.tsx
@@ -1,24 +1,37 @@
 import { useState } from 'react';
 import SearchBar from '../molecules/Search-bar';
 import SearchCardsContainer from '../molecules/Search-cards-container';
+import WeatherDetailView from './Weather-detail-view';
 
 export default function SearchContainer() {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [icon, setIcon] = useState<string>('');
 	const [weatherData, setWeatherData] = useState<any>(null);
+	const [showDetail, setShowDetail] = useState<boolean>(false);
 
 	return (
 		<div className='w-full flex flex-col items-center'>
-			<SearchBar
-				setWeatherData={setWeatherData}
-				setIcon={setIcon}
-				setIsLoading={setIsLoading}
-			/>
-			<SearchCardsContainer
-				icon={icon}
-				isLoading={isLoading}
-				weather={weatherData}
-			/>
+			{!showDetail && (
+				<SearchBar
+					setWeatherData={setWeatherData}
+					setIcon={setIcon}
+					setIsLoading={setIsLoading}
+				/>
+			)}
+
+			{showDetail && weatherData ? (
+				<WeatherDetailView
+					weather={weatherData}
+					icon={icon}
+				/>
+			) : (
+				<SearchCardsContainer
+					icon={icon}
+					isLoading={isLoading}
+					weather={weatherData}
+					onCardClick={() => setShowDetail(true)}
+				/>
+			)}
 		</div>
 	);
 }
