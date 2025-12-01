0c556009bca00ede8328514d60f538da843142bc
Christian Montoya <chrimonlo@gmail.com>
2025-12-01 10:14:18 -0500

    feat(Search-container): change the way to show the detail

--- src/components/organisms/Search-container.tsx
+++ src/components/organisms/Search-container.tsx
@@ -1,37 +1,26 @@
 import { useState } from 'react';
 import SearchBar from '../molecules/Search-bar';
 import SearchCardsContainer from '../molecules/Search-cards-container';
-import WeatherDetailView from './Weather-detail-view';
 
 export default function SearchContainer() {
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const [icon, setIcon] = useState<string>('');
 	const [weatherData, setWeatherData] = useState<any>(null);
-	const [showDetail, setShowDetail] = useState<boolean>(false);
 
 	return (
 		<div className='w-full flex flex-col items-center'>
-			{!showDetail && (
-				<SearchBar
-					setWeatherData={setWeatherData}
-					setIcon={setIcon}
-					setIsLoading={setIsLoading}
-				/>
-			)}
+			<SearchBar
+				setWeatherData={setWeatherData}
+				setIcon={setIcon}
+				setIsLoading={setIsLoading}
+			/>
 
-			{showDetail && weatherData ? (
-				<WeatherDetailView
-					weather={weatherData}
-					icon={icon}
-				/>
-			) : (
-				<SearchCardsContainer
-					icon={icon}
-					isLoading={isLoading}
-					weather={weatherData}
-					onCardClick={() => setShowDetail(true)}
-				/>
-			)}
+			<SearchCardsContainer
+				icon={icon}
+				isLoading={isLoading}
+				weather={weatherData}
+				onCardClick={() => (window.location.href = '/detail')}
+			/>
 		</div>
 	);
 }
