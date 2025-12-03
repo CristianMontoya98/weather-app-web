a26bd01472b01128871ee63f417dbe9640536130
Christian Montoya <chrimonlo@gmail.com>
2025-12-03 16:01:42 -0500

    feat(favorites-icon): Add the onClick prop

--- src/components/atoms/Icon/Favorites-icon.tsx
+++ src/components/atoms/Icon/Favorites-icon.tsx
@@ -5,9 +5,10 @@ type FavoritesIconProps = {
 	height?: number;
 	color?: string;
 	className?: string;
+	onClick?: (event: React.MouseEvent<SVGElement>) => void;
 };
 
-export const FavoritesIcon: React.FC<FavoritesIconProps> = ({ width = 20, height = 19, color = 'currentColor', className }) => {
+export const FavoritesIcon: React.FC<FavoritesIconProps> = ({ width = 20, height = 19, color = 'currentColor', className, onClick }) => {
 	return (
 		<svg
 			width={width}
@@ -18,6 +19,7 @@ export const FavoritesIcon: React.FC<FavoritesIconProps> = ({ width = 20, height
 			className={className}
 			aria-hidden={true}
 			role='img'
+			onClick={onClick}
 		>
 			<path
 				d='M10 18.35L8.55 17.03C3.4 12.36 0 9.27 0 5.5C0 2.41 2.42 0 5.5 0C7.24 0 8.91 0.81 10 2.08C11.09 0.81 12.76 0 14.5 0C17.58 0 20 2.41 20 5.5C20 9.27 16.6 12.36 11.45 17.03L10 18.35Z'
