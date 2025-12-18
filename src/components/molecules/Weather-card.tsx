c35acab499ad28f789c1d03aeb33087fcfc5f228
Christian Montoya <chrimonlo@gmail.com>
2025-12-18 15:58:33 -0500

    fix(Weather-card): Fix the favorite detail bug

--- src/components/molecules/Weather-card.tsx
+++ src/components/molecules/Weather-card.tsx
@@ -6,9 +6,14 @@ interface WeatherCardProps {
 }
 
 export default function WeatherCard({ weather, icon }: WeatherCardProps) {
+	const handleClick = () => {
+		window.location.href = '/detail';
+		localStorage.setItem('searchedWeatherData', JSON.stringify(weather));
+	};
+
 	return (
-		<a
-			href='/detail'
+		<div
+			onClick={handleClick}
 			className='mt-20 cursor-pointer shadow-[5px_7px_6px_0px_rgba(0,_0,_0,_0.1)] bg-gradient-to-br from-slate-900 to-slate-700 p-[14px] rounded-[13px] flex items-center w-[300px] transition-all duration-300 hover:scale-105 hover:-translate-y-2'
 		>
 			<img
@@ -22,6 +27,6 @@ export default function WeatherCard({ weather, icon }: WeatherCardProps) {
 			<div className='w-full h-full flex flex-col items-center justify-center'>
 				<p className='text-[var(--lightBlue)] text-[20px] font-bold ms-5'>{weather?.name}</p>
 			</div>
-		</a>
+		</div>
 	);
 }
