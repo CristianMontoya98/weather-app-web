59f457caaa492e9fdd393bee902b2e63fac39f03
Christian Montoya <chrimonlo@gmail.com>
2026-01-26 17:09:14 -0500

    feat(fetch-weather): Change the way to fetch the data

--- src/services/fetch-weather.ts
+++ src/services/fetch-weather.ts
@@ -1,18 +1,30 @@
 import { mapWeatherToUI } from "../types/weather.mapper";
+import type { WeatherData } from '../types/weather.types';
+import type { WeatherApiError } from '../types/weather.api.types';
+
+export const fetchWeather = async (url: string): Promise<WeatherData> => {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw data as WeatherApiError;
+  }
+
+  return data as WeatherData;
+};
 
 
-export const fetchWeather = (url:string, setData:any, setIcon:any, setIsLoading:any) => {
+/* export const fetchWeather = (url:string, setData:any,setIsLoading:any) => {
     fetch(url)
 				.then((response) => response.json())
 				.then((data) => {
 					setData(mapWeatherToUI(data));
-					console.log(mapWeatherToUI(data));
-					setIcon(data.weather[0].main);
 					const loadingData = setInterval(() => {
 						setIsLoading(false);
 					}, 1000);
 					return () => clearInterval(loadingData);
 				}).catch((error) => {
 					setIsLoading(false);
-				} );
-}
+					console.log(error);
+				});
+} */
