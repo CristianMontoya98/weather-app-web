9bb470a8303ba2ad97895af5c2c9c2121f233797
Christian Montoya <chrimonlo@gmail.com>
2026-01-30 09:55:21 -0500

    fix(api): Fix the http protocol in the api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 const API_KEY = import.meta.env.PUBLIC_OPENWEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
-const GEO_CODING_URL = 'http://api.openweathermap.org/geo/1.0'
+const GEO_CODING_URL = 'https://api.openweathermap.org/geo/1.0'
 
 export const API_ENDPOINTS = {
     CURRENT_WEATHER: (city:string) => `${BASE_URL}/weather?q=${city}&lang=es&appid=${API_KEY}&units=metric&lang=sp,es`,
